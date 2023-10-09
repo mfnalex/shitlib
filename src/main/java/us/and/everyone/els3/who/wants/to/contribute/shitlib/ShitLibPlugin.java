f05251b78bfed284a3b5b7695cfe76d9032a0230
orang3i <86411355+orang3i@users.noreply.github.com>
2023-10-09 14:51:36 +0530

    💩💩💩💩💩💩💩

--- src/main/java/us/and/everyone/els3/who/wants/to/contribute/shitlib/ShitLibPlugin.java
+++ src/main/java/us/and/everyone/els3/who/wants/to/contribute/shitlib/ShitLibPlugin.java
@@ -1,12 +1,5 @@
-package us.and.everyone.els3.who.wants.to.contribute.shitlib;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public class ShitLibPlugin extends JavaPlugin {
-
-
+package us.and.everyone.els3.who.wants.to.contribute.shitlib;import org.bukkit.plugin.java.JavaPlugin;public final class ShitLibPlugin extends JavaPlugin {    
     @Override
     public void onEnable() {
-        System.out.println("ShitLib is enabled!");
-    }
-}
+        System.out.println("shitlib is shitting 💩");
+    }}
